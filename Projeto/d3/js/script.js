63124e09f3399f50de1fd0d61ee75f327a327c4b
Telma Marisa Neto Barragão <telmabarragao@MacBook-Pro-de-Telma.local>
2018-11-17 18:23:16 +0000

    add serach working

--- Projeto/d3/js/script.js
+++ Projeto/d3/js/script.js
@@ -2,6 +2,9 @@ $( document ).ready(function() {
 
       var variableToShow = "EcoFoot";
       var appendTo = "#world_graph";
+      var countries_list = [];
+      var continents_list = [];
+
 
       continents_EcoFoot();
 
@@ -57,8 +60,6 @@ $( document ).ready(function() {
 
 
 
-
-
 ////////////////////D3//////////////////
 
       //Width and height
@@ -107,20 +108,18 @@ $( document ).ready(function() {
 
           data = json.data;
           console.log(data)
-          var count =0;
           var dataArray = new Array();
 
           d3.json("../data/GeoJson.json").then(function(geojson){
 
               datageo = geojson;
-              var wrongCountries = [""];
 
               for(var i = 0; i < data.length; i++){
 
-
                   // grab country name
                   var dataCountry = data[i].country_region;
-                  var wrong=0;
+                  countries_list.push(dataCountry);
+
 
                   //find the corresponding state inside the GeoJSON
                   for(var n = 0; n < datageo.features.length; n++){
@@ -147,9 +146,6 @@ $( document ).ready(function() {
 
                         dataArray.push(totalEcoFootCons);
 
-
-
-
                         //Copy the data value into the JSON
                         // basically creating a new value column in JSON data
                         datageo.features[n].properties.totalBiocapacity = totalBiocapacity;
@@ -163,24 +159,14 @@ $( document ).ready(function() {
                         datageo.features[n].properties.hdi = hdi;
                         datageo.features[n].properties.deficereserve = deficereserve;
 
-
-                        count+=1;
-                        wrong = 0;
-
                         //var datawhat = topojson.feature(data[i], datageo.feautures[n].bbox, datageo.feautures[n].geometry, datageo.feautures[n].properties);
 
                         console.log(datageo)
-
-
                         //stop looking through the JSON
                         break;
 
 
-                      }else if ( dataCountry != jsonState && dataCountry != jsonState1 && dataCountry != jsonState2 && wrong == 254){
-                        wrongCountries.push(dataCountry);
-
                       }else{
-                          wrong+=1;
                       }
                   }
 
@@ -272,8 +258,9 @@ $( document ).ready(function() {
 
         });
 
-      }
+        autocomplete(document.getElementById("myInput"), countries_list);
 
+      }
 
 
       function countries_Biocapacity(){
@@ -290,20 +277,21 @@ $( document ).ready(function() {
 
                 data = json.data;
                 console.log(data)
-                var count =0;
                 var dataArray = new Array();
 
                 d3.json("../data/GeoJson.json").then(function(geojson){
 
                     datageo = geojson;
-                    var wrongCountries = [""];
 
                     for(var i = 0; i < data.length; i++){
 
-
                         // grab country name
                         var dataCountry = data[i].country_region;
-                        var wrong=0;
+                        countries_list.push(dataCountry);
+
+                        console.log(countries_list)
+
+                        autocomplete(document.getElementById("myInput"), countries_list);
 
                         //find the corresponding state inside the GeoJSON
                         for(var n = 0; n < datageo.features.length; n++){
@@ -331,8 +319,6 @@ $( document ).ready(function() {
                               dataArray.push(totalBiocapacity);
 
 
-
-
                               //Copy the data value into the JSON
                               // basically creating a new value column in JSON data
                               datageo.features[n].properties.totalBiocapacity = totalBiocapacity;
@@ -346,25 +332,14 @@ $( document ).ready(function() {
                               datageo.features[n].properties.hdi = hdi;
                               datageo.features[n].properties.deficereserve = deficereserve;
 
-
-                              count+=1;
-                              wrong = 0;
-
                               //var datawhat = topojson.feature(data[i], datageo.feautures[n].bbox, datageo.feautures[n].geometry, datageo.feautures[n].properties);
 
-
                               console.log(datageo)
-
-
                               //stop looking through the JSON
                               break;
 
 
-                            }else if ( dataCountry != jsonState && dataCountry != jsonState1 && dataCountry != jsonState2 && wrong == 254){
-                              wrongCountries.push(dataCountry);
-
                             }else{
-                                wrong+=1;
                             }
                         }
 
@@ -378,10 +353,6 @@ $( document ).ready(function() {
 
                     var ramp = d3.scaleLinear().domain([minVal,10]).range([lowColorB, highColorB])
 
-
-                    console.log(dataArray)
-                    console.log(minVal)
-                    console.log(maxVal)
 
                     //var color_scalee = d3.scaleLinear.quantize().domain([minVal, maxVal]).range(['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']);
                   //  var color = d3.scaleQuantile().domain([0, maxVal]).range(["rgb(237, 248, 233)", "rgb(186, 228, 179)", "rgb(116,196,118)", "rgb(49,163,84)", "rgb(0,109,44)"]);
@@ -457,15 +428,111 @@ $( document ).ready(function() {
               });
 
 
+
       }
 
 
 
+      ////////////////////SEARCH//////////////////
 
+      function autocomplete(inp, arr) {
 
-
-
-
+        console.log("entrei")
+        /*the autocomplete function takes two arguments,
+        the text field element and an array of possible autocompleted values:*/
+        var currentFocus;
+        /*execute a function when someone writes in the text field:*/
+        inp.addEventListener("input", function(e) {
+            var a, b, i, val = this.value;
+            /*close any already open lists of autocompleted values*/
+            closeAllLists();
+            if (!val) { return false;}
+            currentFocus = -1;
+            /*create a DIV element that will contain the items (values):*/
+            a = document.createElement("DIV");
+            a.setAttribute("id", this.id + "autocomplete-list");
+            a.setAttribute("class", "autocomplete-items");
+            /*append the DIV element as a child of the autocomplete container:*/
+            this.parentNode.appendChild(a);
+            /*for each item in the array...*/
+            for (i = 0; i < arr.length; i++) {
+              /*check if the item starts with the same letters as the text field value:*/
+              if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+                /*create a DIV element for each matching element:*/
+                b = document.createElement("DIV");
+                /*make the matching letters bold:*/
+                b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+                b.innerHTML += arr[i].substr(val.length);
+                /*insert a input field that will hold the current array item's value:*/
+                b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+                /*execute a function when someone clicks on the item value (DIV element):*/
+                    b.addEventListener("click", function(e) {
+                    /*insert the value for the autocomplete text field:*/
+                    inp.value = this.getElementsByTagName("input")[0].value;
+                    /*close the list of autocompleted values,
+                    (or any other open lists of autocompleted values:*/
+                    closeAllLists();
+                });
+                a.appendChild(b);
+              }
+            }
+        });
+        /*execute a function presses a key on the keyboard:*/
+        inp.addEventListener("keydown", function(e) {
+            var x = document.getElementById(this.id + "autocomplete-list");
+            if (x) x = x.getElementsByTagName("div");
+            if (e.keyCode == 40) {
+              /*If the arrow DOWN key is pressed,
+              increase the currentFocus variable:*/
+              currentFocus++;
+              /*and and make the current item more visible:*/
+              addActive(x);
+            } else if (e.keyCode == 38) { //up
+              /*If the arrow UP key is pressed,
+              decrease the currentFocus variable:*/
+              currentFocus--;
+              /*and and make the current item more visible:*/
+              addActive(x);
+            } else if (e.keyCode == 13) {
+              /*If the ENTER key is pressed, prevent the form from being submitted,*/
+              e.preventDefault();
+              if (currentFocus > -1) {
+                /*and simulate a click on the "active" item:*/
+                if (x) x[currentFocus].click();
+              }
+            }
+        });
+        function addActive(x) {
+          /*a function to classify an item as "active":*/
+          if (!x) return false;
+          /*start by removing the "active" class on all items:*/
+          removeActive(x);
+          if (currentFocus >= x.length) currentFocus = 0;
+          if (currentFocus < 0) currentFocus = (x.length - 1);
+          /*add class "autocomplete-active":*/
+          x[currentFocus].classList.add("autocomplete-active");
+        }
+        function removeActive(x) {
+          /*a function to remove the "active" class from all autocomplete items:*/
+          for (var i = 0; i < x.length; i++) {
+            x[i].classList.remove("autocomplete-active");
+          }
+        }
+        function closeAllLists(elmnt) {
+          /*close all autocomplete lists in the document,
+          except the one passed as an argument:*/
+          var x = document.getElementsByClassName("autocomplete-items");
+          for (var i = 0; i < x.length; i++) {
+            if (elmnt != x[i] && elmnt != inp) {
+            x[i].parentNode.removeChild(x[i]);
+          }
+        }
+      }
+      /*execute a function when someone clicks in the document:*/
+      document.addEventListener("click", function (e) {
+          closeAllLists(e.target);
+      });
+      }
 
 
 
