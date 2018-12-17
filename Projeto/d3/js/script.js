b19baf053e6ceeeec9838a1b10e96cc2986f237f
Marta Mendes <martamendes@machiavelli.home>
2018-12-17 02:17:18 +0000

    Start modal legend implementation

--- Projeto/d3/js/script.js
+++ Projeto/d3/js/script.js
@@ -3499,3 +3499,33 @@ function round(value, precision){
     var multiplier = Math.pow(10, precision || 0);
     return Math.round(value*multiplier)/multiplier;
 }
+
+//MODAL LEGEND
+
+window.onload=function(){
+  // Get the modal
+  var modal_legend = document.getElementById('modal-legend');
+
+  // Get the button that opens the modal
+  var legend_btn = document.getElementById("legend-button");
+
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+
+  // When the user clicks on the button, open the modal
+  legend_btn.onclick = function() {
+    modal_legend.style.display = "block";
+  }
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+    modal_legend.style.display = "none";
+  }
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal_legend) {
+      modal_legend.style.display = "none";
+    }
+  }
+};
