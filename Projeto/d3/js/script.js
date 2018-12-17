b1533f02c9584ae8f692852893b93a0bd7fb60d6
Telma Marisa Neto Barragão <telmabarragao@MacBook-Pro-de-Telma-3.local>
2018-12-17 12:21:32 +0000

    fix not opening not closing legend

--- Projeto/d3/js/script.js
+++ Projeto/d3/js/script.js
@@ -364,6 +364,31 @@ $( document ).ready(function() {
       continents_EcoFootGha();
       autocomplete(document.getElementById("myInput-continent"), continents_list);
 
+
+
+///////////////////MODAL LEGEND/////////
+
+      // Get the modal
+      var modal_legend = document.getElementById('modal-legend');
+      // Get the button that opens the modal
+      var legend_btn = document.getElementById("legend-button");
+      // Get the <span> element that closes the modal
+      var span = document.getElementsByClassName("close")[0];
+      // When the user clicks on the button, open the modal
+      $("#legend-button").on("click", function() {
+        modal_legend.style.display = "block";
+      });
+      // When the user clicks on <span> (x), close the modal
+      $(".closelegend").on("click", function() {
+        modal_legend.style.display = "none";
+      });
+      // When the user clicks anywhere outside of the modal, close it
+      // window.on("click", function(event) {
+      //   if (event.target == modal_legend) {
+      //     modal_legend.style.display = "none";
+      //   }
+      // });
+
 ////////////////////D3//////////////////
 
       //Width and height
@@ -3506,33 +3531,3 @@ function round(value, precision){
     var multiplier = Math.pow(10, precision || 0);
     return Math.round(value*multiplier)/multiplier;
 }
-
-//MODAL LEGEND
-
-window.onload=function(){
-  // Get the modal
-  var modal_legend = document.getElementById('modal-legend');
-
-  // Get the button that opens the modal
-  var legend_btn = document.getElementById("legend-button");
-
-  // Get the <span> element that closes the modal
-  var span = document.getElementsByClassName("close")[0];
-
-  // When the user clicks on the button, open the modal
-  legend_btn.onclick = function() {
-    modal_legend.style.display = "block";
-  }
-
-  // When the user clicks on <span> (x), close the modal
-  span.onclick = function() {
-    modal_legend.style.display = "none";
-  }
-
-  // When the user clicks anywhere outside of the modal, close it
-  window.onclick = function(event) {
-    if (event.target == modal_legend) {
-      modal_legend.style.display = "none";
-    }
-  }
-};
